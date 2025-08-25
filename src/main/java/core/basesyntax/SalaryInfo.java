f9f6a1cdd7e6b2ba87ae911098417be08550ac90
Denys Funderat <funderatdenis@gmail.com>
2025-08-25 15:26:53 +0300

    constants fix

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -4,7 +4,11 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class SalaryInfo {
-    static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    static final byte INDEX_DATE = 0;
+    static final byte INDEX_NAME = 1;
+    static final byte INDEX_HOURS = 2;
+    static final byte INDEX_RATE = 3;
 
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
         StringBuilder builder = new StringBuilder("Report for period ");
@@ -13,10 +17,10 @@ public class SalaryInfo {
             int totalIncome = 0;
             for (String record : data) {
                 String[] recordValues = record.split(" ");
-                if (name.equalsIgnoreCase(recordValues[1])
-                        && checkDateWithinPeriod(recordValues[0], dateFrom, dateTo)) {
+                if (name.equalsIgnoreCase(recordValues[INDEX_NAME])
+                        && checkDateWithinPeriod(recordValues[INDEX_DATE], dateFrom, dateTo)) {
                     totalIncome +=
-                            Integer.parseInt(recordValues[2]) * Integer.parseInt(recordValues[3]);
+                            Integer.parseInt(recordValues[INDEX_HOURS]) * Integer.parseInt(recordValues[INDEX_RATE]);
                 }
             }
             builder.append(System.lineSeparator()).append(name).append(" - ").append(totalIncome);
@@ -25,9 +29,9 @@ public class SalaryInfo {
     }
 
     private boolean checkDateWithinPeriod(String dateValue, String dateFrom, String dateTo) {
-        LocalDate date = LocalDate.parse(dateValue, formatter);
-        LocalDate dateFromPrep = LocalDate.parse(dateFrom, formatter);
-        LocalDate dateToPrep = LocalDate.parse(dateTo, formatter);
+        LocalDate date = LocalDate.parse(dateValue, DATE_FORMATTER);
+        LocalDate dateFromPrep = LocalDate.parse(dateFrom, DATE_FORMATTER);
+        LocalDate dateToPrep = LocalDate.parse(dateTo, DATE_FORMATTER);
         return (date.isAfter(dateFromPrep) || date.isEqual(dateFromPrep))
                 && (date.isBefore(dateToPrep) || date.isEqual(dateToPrep));
     }
